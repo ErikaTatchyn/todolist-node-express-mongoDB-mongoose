278987438c9a55038af24ce6c59cce0c8db3eabd
Erika Tatchyn <erika.ucu@gmail.com>
2023-06-16 19:00:03 +0100

    add db

--- app.js
+++ app.js
@@ -1,62 +1,69 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
 main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/tasksDB");
 }
-const app = express();
-const date = require(__dirname + "/day");
 
-app.set("view engine", "ejs");
+const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
-const taskSchema = new mongoose.schema({ name: String });
+const taskSchema = new mongoose.Schema({
+  name: String,
+  completed: Boolean,
+});
+
 const Task = mongoose.model("Tasks", taskSchema);
-const task = new Task({ name: "walk" });
-task.save();
 
-// const today = new Date();
-// const day = "";
-// const personalListItems = [];
-// const workListItems = [];
-// const options = {
-//   weekday: "long",
-//   day: "numeric",
-//   month: "long",
-// };
+app.set("view engine", "ejs");
 
-// day = today.toLocaleDateString("en-US", options);
+app.get("/", async function (req, res) {
+  try {
+    const today = new Date();
+    const options = {
+      weekday: "long",
+      day: "numeric",
+      month: "long",
+    };
+    const day = today.toLocaleDateString("en-US", options);
 
-// app.get("/", function (req, res) {
-//   res.render("list", { listTitle: day, newListItem: personalListItems });
-// });
+    const tasks = await Task.find({}, "name").exec();
+    res.render("list", { listTitle: day, newListItem: tasks });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-// app.get("/work", function (req, res) {
-//   res.render("list", { listTitle: "Work List", newListItem: workListItems });
-// });
+app.post("/", async function (req, res) {
+  try {
+    const taskName = req.body.newItem;
+    const task = new Task({ name: taskName, completed: false });
+    await task.save();
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
 
-// app.get("/about", function (req, res) {
-//   res.render("about");
-// });
+app.delete("/tasks/:id", async function (req, res) {
+  try {
+    const taskId = req.params.id;
+    await Task.findByIdAndRemove(taskId);
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
 
-// app.post("/", function (req, res) {
-//   item = req.body.newItem;
-//   buttonType = req.body.button;
-//   if (item === "") {
-//     console.log("Error: New list item is blank");
-//   } else if (buttonType === "Work") {
-//     workListItems.push(item);
-//     res.redirect("/work");
-//   } else {
-//     personalListItems.push(item);
-//     res.redirect("/");
-//   }
-// });
-
-// app.listen(3000, function () {
-//   console.log("Server started on port 3000.");
-// });
+app.listen(3000, function () {
+  console.log("Server started on port 3000.");
+});
